a01950d6c56d4aa4c2fe1c59d350d300ec914da0
LENOVO <amar13617@gmail.com>
2023-06-26 11:50:49 +0530

    file in python committed

--- python_file.py
+++ python_file.py
@@ -10,3 +10,24 @@ my_file_writing = open('data.txt', 'w')
 my_file_writing.write(user_name)
 
 my_file_writing.close()
+
+write_file = open("write_example.txt", "a")
+write_file.write("\nNow you have two lines! You're growing up so fast!")
+
+write_file.close()
+
+#Context managers for working with files.
+
+with open("example.txt", "r") as example_file: #There is no need to close the file.
+    print(example_file.read())
+    
+    
+with open("write_example.txt", "w") as write_file:
+    write_file.write("Welcome to the world, write_example.txt!")
+
+with open("write_example.txt", "a") as write_file:
+    write_file.write("\nNow you have two lines! You're growing up so fast!")
+    
+
+
+
---
+++ working_with_csv.py
@@ -0,0 +1,39 @@
+#with open(r"C:\Users\LENOVO\Desktop\IRIS.csv", "r") as iris_file:
+#    iris_data = iris_file.read().split("\n")
+    
+#print(iris_data)
+
+
+with open(r"C:\Users\LENOVO\Desktop\IRIS.csv", "r") as iris_file:
+    iris_data = iris_file.readlines()
+ 
+irises = []   
+for row in iris_data[1:]:
+    #print((row).split(","))
+    sepal_length, sepal_width, petal_length, petal_width, species = row.strip().split(",")
+    
+    iris_dict = {
+        "sepal_length": sepal_length,
+        "sepal_width": sepal_width,
+        "petal_length": petal_length,
+        "petal_width": petal_width,
+        "species": species
+    }
+
+    irises.append(iris_dict)
+
+print(irises)
+
+
+#EXERCISE
+f = open("hello_world.txt", "w")
+f.write("Hello, World!")
+f.close()
+
+with open("hello_world.txt", "w") as f:
+    f.write("Hello, World!")
+#print(f)
+
+with open("hello_world.txt", "a") as f:
+    f.write("\nHow are you")
+print(f)
